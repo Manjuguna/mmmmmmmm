6707690c8c17891100e5eebd9380296924441534
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-27 13:34:12 +0530

    Update nil.py

--- nil.py
+++ nil.py
@@ -5,7 +5,7 @@ if len(g1)>len(v1):
 p2=0
 while p2<len(g1):
   y+=(ord(v1[p2])-ord(g1[p2]))
-  pt1+=1
-for pt1 in range(p2,len(v1)):
+  p2+=1
+for p2 in range(p2,len(v1)):
   y+=ord(v1[p2])-ord('a')+1
 print(y)
