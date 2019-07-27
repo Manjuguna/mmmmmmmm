358c6cc8ae66c98d7aef13e335ceffa23e630029
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-27 13:31:33 +0530

    Update nil.py

--- nil.py
+++ nil.py
@@ -1,11 +1,11 @@
 g1,v1=map(str,input().split())
-yast=0
+y=0
 if len(g1)>len(v1):
   g1,v1=v1,g1
 p2=0
 while p2<len(g1):
-  yast+=(ord(v1[p2])-ord(g1[p2]))
+  y+=(ord(v1[p2])-ord(g1[p2]))
   pt1+=1
 for pt1 in range(p2,len(v1)):
-  yast+=ord(v1[p2])-ord('a')+1
-print(yast)
+  y+=ord(v1[p2])-ord('a')+1
+print(y)
